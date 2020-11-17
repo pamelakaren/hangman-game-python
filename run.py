f38b5be1555274a0566af0d2d724405eedeed8e3
Isabella Tobias <74330693+tobiasisabella@users.noreply.github.com>
2020-11-17 08:46:21 -0300

    Create run.py

---
+++ run.py
@@ -0,0 +1,149 @@
+import random
+board = ['''
+
+>>>>>>>>>>Hangman<<<<<<<<<<
+
++---+
+|   |
+    |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+|   |
+    |
+    |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|   |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/    |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/ \  |
+     |
+=========''']
+
+
+
+class Hangman:
+
+	
+	def __init__(self, word):
+		self.word = word
+		self.missed_letters = []
+		self.guessed_letters = []
+		
+		
+	
+	def guess(self, letter):
+		if letter in self.word and letter not in self.guessed_letters:
+			self.guessed_letters.append(letter)
+		elif letter not in self.word and letter not in self.missed_letters:
+			self.missed_letters.append(letter)
+		else:
+			return False
+		return True
+		
+		
+	def hangman_over(self):
+		return self.hangman_won() or (len(self.missed_letters) == 6)
+		
+		
+	def hangman_won(self):
+		if '_' not in self.hide_word():
+			return True
+		return False
+		
+
+	def hide_word(self):
+		rtn = ''
+		for letter in self.word:
+			if letter not in self.guessed_letters:
+				rtn += '_'
+			else:
+				rtn += letter
+		return rtn
+		
+		
+	def print_game_status(self):
+		print (board[len(self.missed_letters)])
+		print ('\nWord: ' + self.hide_word())
+		print ('\nWrong letters: ',) 
+		for letter in self.missed_letters:
+			print (letter,) 
+		print ()
+		print ('Correct letters: ',)
+		for letter in self.guessed_letters:
+			print (letter,)
+		print ()
+		
+
+def rand_word():
+        with open("words.txt", "rt") as f:
+                bank = f.readlines()
+        return bank[random.randint(0,len(bank))].strip()
+
+
+
+def main():
+
+	
+	game = Hangman(rand_word())
+
+	
+	while not game.hangman_over():
+		game.print_game_status()
+		user_input = input('\nType a letter: ')
+		game.guess(user_input)
+
+	
+	game.print_game_status()	
+
+	
+	if game.hangman_won():
+		print ('\nCongratulations! You won!!')
+	else:
+		print ('\nGame over! You lost.')
+		print ('The correct word was ' + game.word)
+		
+	print ('\nIt was nice playing with you!\n')
+
+	
+if __name__ == "__main__":
+	main()
